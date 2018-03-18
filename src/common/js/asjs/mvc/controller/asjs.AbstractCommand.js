b6d36dd4824037041e705781a9da64a27c83882d
Gergely Kormendi <kormendi.gergely@gmail.com>
2018-03-18 19:15:31 +0100

    Change

--- src/common/js/asjs/mvc/controller/asjs.AbstractCommand.js
+++ src/common/js/asjs/mvc/controller/asjs.AbstractCommand.js
@@ -1,8 +1,14 @@
-require("../asjs.NotificationDispatcher.js");
+require("../asjs.NotificationHandler.js");
 
 ASJS.AbstractCommand = createClass(
 "AbstractCommand",
-ASJS.NotificationDispatcher,
+ASJS.BaseClass,
 function(_scope) {
+  var _nHandler = ASJS.NotificationHandler.instance;
+
   _scope.execute = function() {}
+
+  _scope.sendNotification = function(type, data) {
+    _nHandler.sendNotification(type, data);
+  }
 });
