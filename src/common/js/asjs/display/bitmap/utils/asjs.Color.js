3ee44d026203c65405f20a5fc1dcb63b637217b2
Gergely Kormendi <kormendi.gergely@gmail.com>
2018-04-04 06:23:38 +0200

    Change

--- src/common/js/asjs/display/bitmap/utils/asjs.Color.js
+++ src/common/js/asjs/display/bitmap/utils/asjs.Color.js
@@ -23,7 +23,7 @@ function(_scope) {
 
 rof(ASJS.Color, "twoColorDistance", function(c1, c2) {
   var d =
-    Math.abs(c1.r - c2.r) + Math.abs(c1.g - c2.g) + 
+    Math.abs(c1.r - c2.r) + Math.abs(c1.g - c2.g) +
     Math.abs(c1.b - c2.b) + Math.abs(c1.a - c2.a);
   return d;
 });
@@ -41,11 +41,20 @@ rof(ASJS.Color, "hexToRgb", function(hex) {
   var result = /^(?:#|0x)?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result ? new ASJS.Color(parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16), 255) : null;
 });
-  
+
 rof(ASJS.Color, "rgbToHex", function(color) {
   var toHex = function(v) {
     var hex = v.toString(16);
     return (hex.length == 1 ? "0" : "") + hex;
   }
   return toHex(color.r) + toHex(color.g) + toHex(color.b);
+});
+
+rof(ASJS.Color, "intToColor", function(num) {
+  num >>>= 0;
+  var b = num & 0xFF,
+      g = (num & 0xFF00) >>> 8,
+      r = (num & 0xFF0000) >>> 16,
+      a = ((num & 0xFF000000) >>> 24) / 255 ;
+  return new ASJS.Color(r, g, b, a);
 });
