1936d7a0fdb4146a61aa9d6bbf7498d2ec0e9cba
asjs-dev <kormendi.gergely@gmail.com>
2020-03-11 20:25:40 +0100

    Change

--- src/common/js/asjs/display/asjs.Scale9Grid.js
+++ src/common/js/asjs/display/asjs.Scale9Grid.js
@@ -36,67 +36,72 @@ createClass(ASJS, "Scale9Grid", ASJS.Sprite, function(_scope, _super) {
     _scope.setCSS("min-width",  imageWidth);
     _scope.setCSS("min-height", imageHeight);
 
-    var right  = imageWidth - rectangle.x - rectangle.width;
-    var bottom = imageHeight - rectangle.y - rectangle.height;
+    var horizontalSize = rectangle.x + rectangle.width;
+    var verticalSize   = rectangle.y + rectangle.height;
+
+    var right  = imageWidth - horizontalSize;
+    var bottom = imageHeight - verticalSize;
+
+    _blocks[0].x = _blocks[3].x = _blocks[6].x =
+    _blocks[0].y = _blocks[1].y = _blocks[2].y = 0;
+
+    _blocks[1].x = _blocks[4].x = _blocks[7].x =
+    _blocks[0].width = _blocks[3].width = _blocks[6].width = rectangle.x;
+
+    _blocks[3].y = _blocks[4].y = _blocks[5].y =
+    _blocks[0].height = _blocks[1].height = _blocks[2].height = rectangle.y;
+
+    _blocks[2].width = _blocks[5].width = _blocks[8].width = right;
+
+    _blocks[6].height = _blocks[7].height = _blocks[8].height = bottom;
+
+    _blocks[1].width = _blocks[4].width = _blocks[7].width = "calc(100% - " + (rectangle.x + right) + "px)";
+
+    _blocks[3].height = _blocks[4].height = _blocks[5].height = "calc(100% - " + (rectangle.y + bottom) + "px)";
+
+    _blocks[2].setCSS("right", 0);
+    _blocks[5].setCSS("right", 0);
+    _blocks[8].setCSS("right", 0);
+
+    _blocks[6].setCSS("bottom", 0);
+    _blocks[7].setCSS("bottom", 0);
+    _blocks[8].setCSS("bottom", 0);
 
     _helperBitmap.setBitmapSize(rectangle.x, rectangle.y);
     _helperBitmap.drawImage(_helperImage, 0, 0, rectangle.x, rectangle.y);
     _blocks[0].src = _helperBitmap.getDataUrl();
-    _blocks[0].move(0, 0);
-    _blocks[0].setSize(rectangle.x, rectangle.y);
 
     _helperBitmap.setBitmapSize(rectangle.width, rectangle.y);
     _helperBitmap.drawImage(_helperImage, rectangle.x, 0, rectangle.width, rectangle.y);
     _blocks[1].src = _helperBitmap.getDataUrl();
-    _blocks[1].move(rectangle.x, 0);
-    _blocks[1].setSize("calc(100% - " + (rectangle.x + right) + "px)", rectangle.y);
 
     _helperBitmap.setBitmapSize(right, rectangle.y);
-    _helperBitmap.drawImage(_helperImage, rectangle.x + rectangle.width, 0, right, rectangle.y);
+    _helperBitmap.drawImage(_helperImage, horizontalSize, 0, right, rectangle.y);
     _blocks[2].src = _helperBitmap.getDataUrl();
-    _blocks[2].setCSS("right", 0);
-    _blocks[2].y = 0;
-    _blocks[2].setSize(right, rectangle.y);
 
     _helperBitmap.setBitmapSize(rectangle.x, rectangle.height);
     _helperBitmap.drawImage(_helperImage, 0, rectangle.y, rectangle.x, rectangle.height);
     _blocks[3].src = _helperBitmap.getDataUrl();
-    _blocks[3].move(0, rectangle.y);
-    _blocks[3].setSize(rectangle.x, "calc(100% - " + (rectangle.y + bottom) + "px)");
 
     _helperBitmap.setBitmapSize(rectangle.width, rectangle.height);
     _helperBitmap.drawImage(_helperImage, rectangle.x, rectangle.y, rectangle.width, rectangle.height);
     _blocks[4].src = _helperBitmap.getDataUrl();
-    _blocks[4].move(rectangle.x, rectangle.y);
-    _blocks[4].setSize("calc(100% - " + (rectangle.x + right) + "px)", "calc(100% - " + (rectangle.y + bottom) + "px)");
 
     _helperBitmap.setBitmapSize(right, rectangle.height);
-    _helperBitmap.drawImage(_helperImage, rectangle.x + rectangle.width, rectangle.y, right, rectangle.height);
+    _helperBitmap.drawImage(_helperImage, horizontalSize, rectangle.y, right, rectangle.height);
     _blocks[5].src = _helperBitmap.getDataUrl();
-    _blocks[5].setCSS("right", 0);
-    _blocks[5].y = rectangle.y;
-    _blocks[5].setSize(right, "calc(100% - " + (rectangle.y + bottom) + "px)");
 
     _helperBitmap.setBitmapSize(rectangle.x, bottom);
-    _helperBitmap.drawImage(_helperImage, 0, rectangle.y + rectangle.height, rectangle.x, bottom);
+    _helperBitmap.drawImage(_helperImage, 0, verticalSize, rectangle.x, bottom);
     _blocks[6].src = _helperBitmap.getDataUrl();
-    _blocks[6].x = 0;
-    _blocks[6].setCSS("bottom", 0);
-    _blocks[6].setSize(rectangle.x, bottom);
 
     _helperBitmap.setBitmapSize(rectangle.width, bottom);
-    _helperBitmap.drawImage(_helperImage, rectangle.x, rectangle.y + rectangle.height, rectangle.width, bottom);
+    _helperBitmap.drawImage(_helperImage, rectangle.x, verticalSize, rectangle.width, bottom);
     _blocks[7].src = _helperBitmap.getDataUrl();
-    _blocks[7].x = rectangle.x;
-    _blocks[7].setCSS("bottom", 0);
-    _blocks[7].setSize("calc(100% - " + (rectangle.x + right) + "px)", bottom);
 
     _helperBitmap.setBitmapSize(right, bottom);
-    _helperBitmap.drawImage(_helperImage, rectangle.x + rectangle.width, rectangle.y + rectangle.height, right, bottom);
+    _helperBitmap.drawImage(_helperImage, horizontalSize, verticalSize, right, bottom);
     _blocks[8].src = _helperBitmap.getDataUrl();
-    _blocks[8].setCSS("right", 0);
-    _blocks[8].setCSS("bottom", 0);
-    _blocks[8].setSize(right, bottom);
 
     _helperBitmap.destroy();
   }
